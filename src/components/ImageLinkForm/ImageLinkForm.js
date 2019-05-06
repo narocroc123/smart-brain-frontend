6fc094b4b32b03d1b8971bf39faa784f8216567d
narocroc123 <42586341+narocroc123@users.noreply.github.com>
2019-05-06 19:33:27 +0100

    Updating Usability

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,11 +8,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
                 {'This Magic Brain will detect faces in your pictures.'}
             </p>
             <p className='f3'>
-                {'Give it a try by pasting a .jpg image URL below!'}
+                {'Give it a try by pasting an image URL below!'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder="Paste URL here..."/>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder="Paste .jpg URL here..."/>
                     <button
                         className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                         onClick={onButtonSubmit}
